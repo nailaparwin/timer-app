54b9864b2c761317d351c1d00dd1a91629ec4090
naila <nailaparwin@gmail.com>
2020-09-21 19:32:26 +0500

    Update Controls.tsx

--- src/components/Controls/Controls.tsx
+++ src/components/Controls/Controls.tsx
@@ -6,22 +6,28 @@ type Props = {
 }
 function Controls(props: Props) {
     const { setTimeInSeconds } = props;
+    const [isOn, setIsOn] = useState(false);
     const [intervalId, setIntervalId] = useState<number>(0);
 
     const handlePlayButton = () => {
+        if (!isOn){
         let interval: any = setInterval(() => {
             setTimeInSeconds((previousState: number) => previousState+1);
         }, 1000);
+        setIsOn(true);
         setIntervalId(interval);
+        }
     }
 
     const handleStopButton = () => {
         clearInterval(intervalId);
+        setIsOn(false);
     }
 
     const handleResetButton = () => {
         clearInterval(intervalId);
         setTimeInSeconds(0);
+        setIsOn(false);
     }
     
     return (
